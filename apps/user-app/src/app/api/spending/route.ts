634019b1777b28a93c008e0ecac1bb3a39ef82bc
Muhammad Suleman <tech4you330@gmail.com>
2025-09-28 13:06:34 +0500

    activity chart fix

--- apps/user-app/src/app/api/spending/route.ts
+++ apps/user-app/src/app/api/spending/route.ts
@@ -1,18 +1,25 @@
 import { NextResponse } from "next/server";
 import prisma from "@repo/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
 
 export async function GET() {
+  const userSession = await getServerSession(authOptions);
+  
   try {
     const offRamps = await prisma.offRampTransaction.findMany({
       select: { amount: true, startTime: true },
+      where: {userId: Number(userSession.user.id)}
     });
 
     const p2pTransfers = await prisma.p2pTransfer.findMany({
       select: { amount: true, timestamp: true },
+      where: {fromUserId: Number(userSession.user.id)}
     });
 
     const onRamps = await prisma.onRampTransaction.findMany({
       select: { amount: true, startTime: true },
+      where: {userId: Number(userSession.user.id)}
     });
 
     // ✅ Weeks structure

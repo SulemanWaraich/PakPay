188dbe2ece68b9b88759061ba659543187261958
Muhammad Suleman <tech4you330@gmail.com>
2025-10-05 15:43:00 +0500

    payment section added

---
+++ apps/user-app/src/app/api/create-onramp/route.ts
@@ -0,0 +1,35 @@
+// app/api/create-offramp/route.ts
+import { NextResponse } from "next/server";
+import prisma from "@repo/db"; // your prisma client
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+
+export async function POST(req: Request) {
+    try {
+        const { amount, bank } = await req.json();
+        const session = await getServerSession(authOptions);
+        if (!session.user) {
+          return NextResponse.json({ success: false, error: "User not logged in" }, { status: 401 });
+        }
+
+
+        const token = String(Math.random() * 100);
+
+        // create a withdrawal record in DB
+        const transaction = await prisma.onRampTransaction.create({
+            data: {
+                amount,
+                provider: bank,
+                status: "Processing",
+                startTime: new Date(),
+                token,
+                userId: Number(session?.user?.id)
+            },
+        });
+
+        return NextResponse.json({ success: true, transaction });
+    } catch (error) {
+        console.error("create-onramp error:", error);
+        return NextResponse.json({ success: false, error: "Failed to create onramp" }, { status: 500 });
+    }
+}

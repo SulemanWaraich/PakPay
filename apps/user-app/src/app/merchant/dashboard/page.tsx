b78abb1106115e600ea5abace95a95a02f1c1737
Muhammad Suleman <tech4you330@gmail.com>
2026-02-05 20:47:35 +0500

    login checkpoint

--- apps/user-app/src/app/merchant/dashboard/page.tsx
+++ apps/user-app/src/app/merchant/dashboard/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { Card, CardContent } from "../../../components/ui/card"
 import prisma from "@repo/db"
 import ActivityChart from "../../../components/ChartMerchant"

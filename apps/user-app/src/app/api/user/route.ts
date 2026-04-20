56d80ee483e7a0c77bf67c39cded3d2315bfc2c5
Muhammad Suleman <tech4you330@gmail.com>
2026-04-20 08:36:01 +0500

    fix: MerchantCategory enum validation in /api/qr

--- apps/user-app/src/app/api/user/route.ts
+++ apps/user-app/src/app/api/user/route.ts
@@ -1,17 +1,28 @@
 import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server"
 import { authOptions } from "../../lib/auth"
+import prisma from "@repo/db"
+
 
 export const GET = async () => {
   const session = await getServerSession(authOptions)
 
-  if(session.user){
-    return NextResponse.json({
-      user: session.user
-    })
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  const user = await prisma.user.findUnique({
+    where: { id: Number(session.user.id) },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+    }
+  })
+
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 })
   }
 
-  return NextResponse.json({
-    message: "You are not logged in"
-  }, {status: 403})
+  return NextResponse.json({ user })
 }
